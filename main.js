67b2c248f4642daf209e4468a21ee786f76d0cc3
Sid <71117904+epicgamerXDLOL@users.noreply.github.com>
2021-02-06 16:55:13 -0700

    finished roguh draft of the website

---
+++ main.js
@@ -0,0 +1,17 @@
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "250px";
+  }
+
+  function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+  }
+
+function openHelpServer()
+{
+    window.open("https://discord.gg/zDCufSRPyM")
+}
+
+function openInvite()
+{
+    window.open("https://discord.com/oauth2/authorize?client_id=778716085650325536&scope=bot&permissions=8")
+}
